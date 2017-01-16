d6fc5e417a2bcdeb5a211d085569ae4b5b4a28d2
Kelly Brandon <kelly.deckelman@gmail.com>
2017-01-16 11:41:28 -0500

    p5 project with template files
    Added p5 project with template files for background, character, and
    obstacle

---
+++ game/obstacle.js
@@ -0,0 +1,34 @@
+/*This obstacle.js file will hold the functions 
+ * that intialize and udpate the obstacle
+ *
+ *File description:
+ *ADD HERE
+ *
+ */
+
+var x;
+
+function initializeObstacle() {
+  x = ...;
+}
+
+function updateObstacle() {
+  //insert code the make the obstacle move automatically
+  //and reset to random positions so the game isn't boring.
+
+  //insert the code that draws the obstacle
+
+}
+
+/** TESTING CODE **/
+/* This code should be commented when the team has put all
+the files in one project. But you will need this code to test
+the character functions. Add the code to test your functions in here.
+*/
+function setup() {
+  
+}
+
+function draw() {
+
+}
